51b5174f4ff64b72083ee906dc6e43f9b3cddd9f
ALAN GABRIEL GORDON <48860730@ort>
2026-03-18 10:37:40 -0300

    terminando el tp

--- index.js
+++ index.js
@@ -25,14 +25,20 @@ else if(c > a && c > b){
 
 //ejercicio 4
 function validarPassword(password){
-    let contraseñaPosible = false
-    if(password.length >= 8 && /[0-9]/.test(password)){
-        contraseñaPosible = true;
-
+    if(password.length >= 8 ){
+    for(let i = 0; i < password.length; i++){
+    for(let j = 0; i < 10; j++){
+    if(password[i] === j){
+        return true;
     }
-
-    return contraseñaPosible
 }
+}
+}
+
+    return false
+}
+
+console.log(validarPassword("jhgadh1hdh"))
 
 //ejercicio 5
 function sumaArray(numeros){
@@ -84,3 +90,133 @@ function activarUsuario(usuario){
 }
 
 //ejercicio 10
+
+
+function calcularPrecio(productos){
+let precioTotal = 0;
+for(let i = 0; i < productos.length; i++){
+    precioTotal += productos[i].precio;
+}
+
+return precioTotal;
+}
+
+
+//ejercicio 11
+
+function nombresUsuarios(usuarios){
+
+let nombreUsuarios = usuarios.map(e => e.nombre)
+
+return nombreUsuarios;
+
+}
+
+//ejercicio 12
+
+function usuariosMay(usuarios){
+
+    let usuariosMayore = usuarios.filter(e => e.edad >= 18)
+
+    return usuariosMayore
+
+}
+
+//ejercicio 13
+
+function sumaEdades(usuarios){
+
+    let edades = usuarios.reduce((total,edad) => total + edad, 0)
+
+    return edades
+
+}
+
+//ejercicio 14 no lo vimos
+
+function extraerVar(productos){
+
+
+
+}
+
+// ejercicio 15 no lo vimos 
+
+// ejercicio 16
+
+function buscarProducto(productos, nombre){
+
+    let producto = productos.filter(e => e.nombre === nombre)
+
+    return producto
+
+
+}
+
+//ejercicio 17
+
+function productoM(productos){
+
+    let productosM = productos.filter(e => e.precio += precio >50)
+
+    return productosM
+
+}
+
+//ejercicio 18
+
+function promedio(numeros){
+
+    let notaNum = numeros.num
+    let cantidadNum = numeros.length
+    let promedioNum = notaNum / cantidadNum
+
+    return promedioNum
+}
+
+//ejercicio 19 
+
+
+const usuarios = [
+
+{id:1, nombre:"Ana", edad:20},
+
+{id:2, nombre:"Juan", edad:15},
+
+{id:3, nombre:"Pedro", edad:30}
+
+]
+
+function obtenerUsuarios(){
+    return usuarios
+}
+
+//ejercicio 20 
+
+function obtenerUsuarioPorId(id){
+
+    let usuarioM = usuarios.filter(e => e.id === id)
+    
+    return usuarioM;
+    
+}
+
+//ejercicio 21
+
+function obtenerMayores(){
+    
+    let usuariosMay = usuarios.filter(e => e.edad >= 18)
+
+    return usuariosMay
+}
+
+//ejercicio 22
+
+function agregarUsuario(nombre, edad){
+
+    usuarios.push({id: usuarios.length + 1, nombre: nombre, edad: edad})
+    
+}
+
+
+
